8023a715e942e69369781525d9ea256fdccbf2fc
vendaJackson <mulwelifx45@gmail.com>
2025-09-13 14:15:18 +0200

    User Login routes done

--- src/Backend_api/index.js
+++ src/Backend_api/index.js
@@ -1,23 +1,27 @@
 import express from "express";
-import dotenv from "dotenv";
 import admin from "firebase-admin";
 import { v4 as uuidv4 } from "uuid";
 
-
-dotenv.config();
-//#region initialise and setup
+// #region Load environment variables
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
+const privateKey = "fhf7fjJjdhfjvnG1123"
+// #endregion
 
+// #regionInitialize Firestore with service account
 admin.initializeApp({
-  credential: admin.credential.cert(
-    JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
-  ),
+  credential: admin.credential.cert("src/Backend_api/safepay-connect-firebase-adminsdk-fbsvc-8175ce9093.json"),
 });
-//#endregion
 
+const db = admin.firestore();
+// #endregion
 // #region Routes
 // #region User routes
+app.get("/", (req, res) => {
+  res.send("Express + Firestore API running 🚀");
+});
+
 app.post("/register", async (req, res) => {
     try {
         const { name, surname, username, phoneNumber, password, email } = req.body;
@@ -61,7 +65,49 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post("/login", async (req, res) => {});
+app.post("/login", async (req, res) => {
+  try {
+
+    const { email, password } = req.body;
+    // retrieves the user corresponding to the email
+    const userQuery = await db.collection("users").where("email", "==", email).get();
+    if (userQuery.empty) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // retrieves the userID from that user
+    const userDoc = userQuery.docs[0];
+    const userData = userDoc.data();
+    const userId = userDoc.id;
+    const keyDoc = await db.collection("keys").doc(userId).get();
+    if (!keyDoc.exists) {
+      return res.status(500).json({ message: "Security info missing" });
+    }
+
+    // finds the password and checks if the password matched the password in the database
+    const { password: hashedPassword } = keyDoc.data();
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // generates and updates the AUthToken after every login
+    const token = authTokenGenerator(userId, userData.name, userData.surname);
+    await db.collection("keys").doc(userId).update({authToken: token});
+    res.status(200).json({
+      userId,
+      username:userData.username,
+      name: userData.name,
+      surname: userData.surname,
+      email: userData.email,
+      token
+    });
+    console.log("User: ", userId, " Has logged In");
+
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
 // #endregion
 
 // #endregion
